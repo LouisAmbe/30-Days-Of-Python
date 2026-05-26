9b6d0e2ad8a8f374db222fdd3dcd482b36ee56ab
Louis Ambe <figolamar2@gmail.com>
2026-05-26 00:03:48 -0500

    Update variables.py

--- Day_2/variables.py
+++ Day_2/variables.py
@@ -1,1 +1,28 @@
+# Day 2: 30 Days of python programming
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + last_name
+country = "USA"
+city = "New York"
+age = 20
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+speed, acc, velocity = 12, 23, 34
 
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(tyep(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(speed))
+print(type(acc))
+print(type(velocity))
+
+print(len(first_name))
